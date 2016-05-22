0b0faea41507c10ba765ae4761f782abf99759dc
Fred <fenglei.gu@gmail.com>
2016-05-22 19:55:20 +1000

    Update using_ALS.py

--- using_ALS.py
+++ using_ALS.py
@@ -29,6 +29,51 @@ def get_training_file_name():
 def get_testing_file_name():
     return output_path+'test_ratings_'+str(start_time.strftime("%Y-%m-%d-%H-%M")) + '.csv'
 
+def get_rdd_output():
+    return output_path+'rdd_output_'+str(start_time.strftime("%Y-%m-%d-%H-%M")) + '.csv'
+
+def transform_test_file():
+    print('Transforming test file...')
+    f = open(test_file,'r')
+    #read the title and ignore
+    f.readline()
+  
+    test_file_name = get_testing_file_name()
+    out  = open(test_file_name,"w")    
+    
+    total = 0
+    while 1:
+        line = f.readline().strip()
+        total+=1
+        #we just read 100 lines temporarily
+        #if total % 100 == 0:
+        #    break        
+        
+        if total % 1000000 == 0:
+            print('Read {} lines...'.format(total))
+            
+        if line == '':
+            break
+        
+        arr = line.split(",")
+        id = arr[0]
+        user_location_city = arr[6]
+        orig_destination_distance = arr[7]
+        srch_destination_id = arr[17]
+        hotel_country = arr[20]
+        hotel_market = arr[21]
+        user_id = arr[8]
+        srch_adults_cnt=int(arr[14])
+        srch_children_cnt=int(arr[15])
+        srch_rm_cnt=int(arr[16])
+    
+        # write to test_file
+
+        out.write(user_id+","+srch_destination_id+","+"1.0"+"\n" )
+        
+    f.close()
+    out.close()
+            
 def transform_train_file():
         
     print('Transforming train file...')
@@ -42,8 +87,8 @@ def transform_train_file():
         line = f.readline().strip()
         total+=1
         #we just read 100 lines temporarily
-        if total % 100 == 0:
-            break
+        #if total % 100 == 0:
+        #    break
         
         if total % 10000000 == 0:
             print('Read {} lines...'.format(total))
@@ -71,14 +116,15 @@ def transform_train_file():
         if user_id != '' and srch_destination_id != '':
             user_item_rating[(user_id, srch_destination_id)][hotel_cluster] += append_1
         
-        if user_location_city != '' and orig_destination_distance != '' :
-            best_hotels_od_ulc[(user_location_city, orig_destination_distance)][hotel_cluster] += 1
+        #if user_location_city != '' and orig_destination_distance != '' :
+        #    best_hotels_od_ulc[(user_location_city, orig_destination_distance)][hotel_cluster] += 1
     
     f.close()
     #convert dictionary to RDD
     
-    path = output_path+'train_ratings_'+str(start_time.strftime("%Y-%m-%d-%H-%M")) + '.csv'
-    out  = open(path,"w")
+    #path = output_path+'train_ratings_'+str(start_time.strftime("%Y-%m-%d-%H-%M")) + '.csv'
+    train_file_name = get_training_file_name()
+    out  = open(train_file_name,"w")
     #out.write("user_id, location, ratings\n")   #NO title
     
     for user_id, srch_destination_id in user_item_rating:
@@ -93,9 +139,12 @@ def transform_train_file():
             out.write(user_id+","+srch_destination_id+","+ cluster_id+"\n" )
             
             break
-        
+    out.close()
     #print(rating_train)
-    
+
+def toCSVLine(data):
+    return ','.join(str(d) for d in data)
+
 def execute_recommendation():
 
     sc = SparkContext(appName="PythonCollaborativeFilteringExample")
@@ -103,15 +152,38 @@ def execute_recommendation():
     
     #Load train data and train
     train_file_name = get_training_file_name()
-    train_data = sc.textFile(train_file_name )
-    ratings = data.map(lambda l: l.split(','))\
+    train_data = sc.textFile(train_file_name)
+    ratings = train_data.map(lambda l: l.split(','))\
         .map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
     
     rank = 10
     number_iteration = 10
     model = ALS.train(ratings, rank, number_iteration)
     
+    #load test data and do prediction 
+    test_file_name = get_testing_file_name()
+    test_data = sc.textFile(test_file_name)
+    test_ranking=test_data.map(lambda l: l.split(','))\
+        .map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
     
+    
+    testdata = test_ranking.map(lambda p: (p[0], p[1]))
+    count_rdd=testdata.count()
+    print("testdata count_rdd=",count_rdd)
+    if count_rdd > 0:
+        predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+        
+        #predictions_lines = predictions.map(toCSVLine)
+        result_file = get_rdd_output()
+        predictions.saveAsTextFile(result_file)
+    
+        count_rdd = predictions.count()
+        print("after prediction: count_rdd=",count_rdd)
+    else:
+        print("Error: empty testdata")
+
+    sc.stop()
     
 transform_train_file()
+transform_test_file()
 execute_recommendation()
